2a1c83a940250457e722bdc4b4a075cd88fa6924
TebaNady <teba7033@gmail.com>
2023-09-28 05:02:22 +0300

    Add the interface

--- MoveApp/src/app/movies/all-movies/all-movies.component.ts
+++ MoveApp/src/app/movies/all-movies/all-movies.component.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
 import { MoviesService } from '../services/movies.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-all-movies',
   templateUrl: './all-movies.component.html',
   styleUrls: ['./all-movies.component.css']
 })
 export class AllMoviesComponent {
+  
   movies: any[] = []
-  constructor(private service: MoviesService) {}
+  constructor(private service: MoviesService, private router : Router) {}
   ngOnInit() {
     this.getMovies()
   }
   getMovies() {
     this.service.getAllMovies().subscribe((res: any) => {
       this.movies = res.results
-      console.log(res)
     })
+  }
+  goToMovie(id : number){
+    this.router.navigate(['details',id])
   }
 }
