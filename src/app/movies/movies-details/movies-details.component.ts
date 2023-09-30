cd07d8f458cd1f8efc30ca28bd0e1168e612a2a3
TebaNady <teba7033@gmail.com>
2023-09-30 06:45:15 +0300

    Adding the design of movie details page

--- src/app/movies/movies-details/movies-details.component.ts
+++ src/app/movies/movies-details/movies-details.component.ts
@@ -9,6 +9,37 @@ import { Movies } from 'src/app/shared/interfaces/movies';
   styleUrls: ['./movies-details.component.css']
 })
 export class MoviesDetailsComponent implements OnInit {
+  languages = [
+    { prefix: 'en', name: 'English' },
+    { prefix: 'fr', name: 'French' },
+    { prefix: 'es', name: 'Spanish' },
+    { prefix: 'pt', name: 'Portuguese' },
+    {prefix: 'is', name: 'Icelandic'},
+    {prefix: 'it', name: 'Italian'},
+    {prefix: 'ru', name: 'Russian'}
+  ];
+  countries = [
+    { prefix: 'IT', name: 'Italy' },
+    { prefix: 'US', name: 'United States of America' },
+    { prefix: 'CN', name: 'China' },
+    { prefix: 'DE', name: 'Germany' },
+    {prefix: 'IS', name: 'Iceland'},
+    {prefix: 'AE', name: 'United Arab Emirates'},
+    {prefix: 'FR', name: 'France'},
+    {prefix: 'CH', name: 'Switzerland'},
+    {prefix: 'GB', name: 'United Kingdom'},
+    {prefix: 'AU', name: 'Australia'},
+    {prefix: 'FR', name: 'France'},
+  ]
+
+  getLanguageName(prefix: string): string {
+    const language = this.languages.find(lang => lang.prefix === prefix);
+    return language ? language.name : 'Unknown';
+  }
+  getCountryName(prefix: string): string {
+    const country = this.countries.find(coun => coun.prefix === prefix);
+    return country ? country.name : 'Unknown';
+  }
   selectedMovie: Movies | undefined;
   idActive: number | undefined;
 
