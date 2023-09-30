9d4bd10005c8327bb8bb7a1e6cbdafb73afd60c4
Mostafa-Rabea <mostafa.rapi3@gmail.com>
2023-09-30 20:01:46 +0300

    Watchlist functionality: updated function and design of the page

--- src/app/movies/watchlist/watchlist.component.ts
+++ src/app/movies/watchlist/watchlist.component.ts
@@ -12,6 +12,7 @@ export class WatchlistComponent {
   watchlistMovies: any[] = [];
 
 
+
   constructor(private service: MoviesService){}
 ngOnInit(){
   let watchlistItems:any = localStorage.getItem('watchlist') || []
@@ -27,6 +28,22 @@ ngOnInit(){
     this.service.getMovie(movie).subscribe((movie) => {
       console.log(movie);
       this.watchlistMovies.push(movie);})
+    }
+  }
+
+  deleteFromWatchlist(movieId:number){
+    console.log(movieId);
+    //? Delete movie on the page
+    this.watchlistMovies.splice(this.watchlistMovies.findIndex(movie => movie.id === movieId), 1)
+
+    //? Delete from local storage
+    let watchlistArray = JSON.parse(localStorage.getItem('watchlist') ?? '[]') as number[];
+    console.log(watchlistArray);
+  
+    const index = watchlistArray.findIndex(id => id === movieId);
+    if (index !== -1) {
+      watchlistArray.splice(index, 1);
+      localStorage.setItem('watchlist', JSON.stringify(watchlistArray));
     }
   }
 }
